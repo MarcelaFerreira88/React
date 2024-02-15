84c354173f9c842698da01badadd56e8d396b31c
Marcela Ferreira <maferre.marcela@gmail.com>
2024-02-15 12:32:09 -0300

    Alterando aspas nos links

--- src/pages/SobrePage/SobreMimPage/index.jsx
+++ src/pages/SobrePage/SobreMimPage/index.jsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 
 const SobreMimPage = () => {
